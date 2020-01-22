985ceaea5f6d0f8f7768fc3a7363440710b7185b
kohei-jp <kohei.mitateda@gmail.com>
2020-01-22 10:54:58 +0900

    message_Async_com

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,63 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      var html = 
+            `<div class="main-chat__message-list__unit">
+              <div class="main-chat__message-list__unit__data">
+                <div class="main-chat__message-list__unit__data--name">
+                  ${message.user_name}
+                </div>
+                <div class="main-chat__message-list__unit__data--date">
+                  ${message.date}
+                </div>
+              </div>
+              <div class="main-chat__message-list__unit__comment--cont">
+                ${message.content}
+              </div>
+              <img src=${message.image} >
+            </div>`
+      return html;
+    } else {
+      var html =
+            `<div class="main-chat__message-list__unit">
+              <div class="main-chat__message-list__unit__data">
+                <div class="main-chat__message-list__unit__data--name">
+                  ${message.user_name}
+                </div>
+                <div class="main-chat__message-list__unit__data--date">
+                  ${message.date}
+                </div>
+              </div>
+              <div class="main-chat__message-list__unit__comment--cont">
+                ${message.content}
+              </div>
+            </div>`
+      return html;
+    };
+  }
+
+
+  $("#new_message").on("submit", function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,  //同期通信でいう『パス』
+      type: 'POST',  //同期通信でいう『HTTPメソッド』
+      data: formData,  
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $(".main-chat__message-list").append(html);
+      $(".main-chat__message-list").animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
+      $(".new_message")[0].reset();
+      $(".submit-btn").prop("disabled",false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+  });
+  })
+});
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,18 +4,14 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
-    # binding.pry
   end
-  # 疑問 messagesコントローラ内なのにgroupから始める?結局何のデータを取りたいの? assoしてればok
-  # →include(:user)って何目的?  n+1が発生してしまう
-  #→グループに所属する全てのメッセージ(何に使う?)
-
-  #@groupってどこからきた?→before_acgionのやつ
 
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+      respond_to do |format|
+        format.json
+      end
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
@@ -31,6 +27,5 @@ class MessagesController < ApplicationController
   def set_group
     @group = Group.find(params[:group_id])
   end
-  #→messageコントローラ内で普通にgroupのアクティブレコードのメソッド使えた?
 
 end
