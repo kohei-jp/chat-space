d47cf02db9cc4493ee9fdad650555dbc63d4f74b
kohei-jp <kohei.mitateda@gmail.com>
2020-01-26 16:05:57 +0900

    cap error2

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,6 +28,7 @@ set :keep_releases, 5
 # secrets.yml用のシンボリックリンクを追加
 set :linked_files, %w{ config/secrets.yml }
 
+# デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
